d3a19f2982c94e825985e1eb54769b41a8fc19f6
joshcarp <joshuacarpeggiani@gmail.com>
2021-02-17 12:40:15 +1100

    Add initial version

---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"reflect"
+	"testing"
+)
+
+func TestStruct(t *testing.T) {
+	type Bar struct {
+		Arr []int
+	}
+	type Foo struct {
+		A Bar
+	}
+	ElementsMatchRec(t,Foo{A: Bar{Arr: []int{1, 2, 3}}}, Foo{A: Bar{Arr: []int{3, 2, 1}}})
+}
+
+func TestPointer(t *testing.T) {
+	type Bar struct {
+		Arr []int
+	}
+	type Foo struct {
+		A *Bar
+	}
+	ElementsMatchRec(t,&Foo{A: &Bar{Arr: []int{1, 2, 3}}}, &Foo{A: &Bar{Arr: []int{3, 2, 1}}})
+}
+
+func TestArray(t *testing.T) {
+	ElementsMatchRec(t, []int{1, 2, 3}, []int{3, 2, 1})
+}
+
+func TestPrimitive(t *testing.T) {
+	ElementsMatchRec(t, 1, 1)
+}
